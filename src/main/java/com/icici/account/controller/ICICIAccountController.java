57ff13ecc6238cf4504e2f84a2c92dc48668e44d
Sai Kumar <saiteja.tkr07@gmail.com>
2023-08-26 23:19:02 +0530

    initialize logs in ICICIAcountController.java file

--- src/main/java/com/icici/account/controller/ICICIAccountController.java
+++ src/main/java/com/icici/account/controller/ICICIAccountController.java
@@ -2,6 +2,8 @@ package com.icici.account.controller;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +21,11 @@ import com.icici.account.repository.ICICIAccountRepo;
 @RestController
 @RequestMapping("/icici/account")
 public class ICICIAccountController {
+	
+	Logger logger = LoggerFactory.getLogger(ICICIAccountController.class);
 
 	@Autowired
 	private ICICIAccountRepo iciciAccountRepo;
-	
 
 	@PostMapping("/create")
 	public ResponseEntity<ICICIAccountDetails> createAccount(@RequestBody ICICIAccountDetails iciciAccount) {
@@ -32,12 +35,17 @@ public class ICICIAccountController {
 	
 	@GetMapping("/getAccount/{accountNo}")
 	public ResponseEntity<ICICIAccountDetails> getAccountDetails(@PathVariable String accountNo) throws ICICIAccountNotFoundException{
+		logger.info("Entered into getAccountDetails method");
 		Optional<ICICIAccountDetails> hdfcAccount = iciciAccountRepo.findById(accountNo);
 		if(hdfcAccount.isPresent()) {
+			logger.info("Exiting from getAccountDetails method");
 			return new ResponseEntity<ICICIAccountDetails>(hdfcAccount.get(),HttpStatus.OK);
 		}else {
+			logger.error(String.format("Account Details not found for the provided Account Number %s", accountNo));
 			throw new ICICIAccountNotFoundException(String.format("Account Details not found for the provided Account Number %s", accountNo));
+			
 		}
+		
 		
 	}
 
